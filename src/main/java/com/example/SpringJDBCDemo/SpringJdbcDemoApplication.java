01da954ff8c3663eec34ab7b3f3da9eb9abb743e
karan <karanssb@gmail.com>
2024-12-13 16:57:35 -0500

    testing

--- src/main/java/com/example/SpringJDBCDemo/SpringJdbcDemoApplication.java
+++ src/main/java/com/example/SpringJDBCDemo/SpringJdbcDemoApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class SpringJdbcDemoApplication {
 
@@ -20,6 +25,27 @@ public class SpringJdbcDemoApplication {
 		TabletestRepo repo = context.getBean(TabletestRepo.class);
 		repo.save(tabletest1);
 		System.out.println(repo.findAll());
+
+		List<String> list = Arrays.asList("hello", "world",null);
+		List<String> upperCaseList = list.stream()
+				.filter(s -> s != null)
+				.map(String::toUpperCase)
+				.collect(Collectors.toList());
+
+		System.out.println(upperCaseList);
+		List<Integer> numbers = new ArrayList<>();
+		for(int i=0;i<=1000;i++) {
+			numbers.add(i);
+		}
+/*
+		for (int num :a) {
+			System.out.println(num + " ");}
+*/
+		long count = numbers.parallelStream()
+				.filter(n -> n > 100) // Correct filter condition
+				.count();
+		System.out.println("Count of numbers greater than 100: " + count);
+
 
 	}
 }
